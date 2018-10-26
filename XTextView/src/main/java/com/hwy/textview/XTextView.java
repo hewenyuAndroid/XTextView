7574bd7cd3efd3db1a66bbafe29f78aae8c3463e
hewenyu <he875027547@126.com>
2018-10-26 11:49:00 +0800

    初始化项目

---
+++ XTextView/src/main/java/com/hwy/textview/XTextView.java
@@ -0,0 +1,26 @@
+package com.hwy.textview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * 作者: hewenyu
+ * 日期: 2018/10/25 21:46
+ * 说明: TextView 封装
+ */
+public class XTextView extends TextView {
+
+    public XTextView(Context context) {
+        super(context);
+    }
+
+    public XTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public XTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
