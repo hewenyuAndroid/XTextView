3e836be050625449954688a7888fef2e47197eed
hewenyu <he875027547@126.com>
2018-10-31 18:04:03 +0800

    增加XEditText GIF

--- app/src/main/java/com/hwy/example/EditActivity.java
+++ app/src/main/java/com/hwy/example/EditActivity.java
@@ -1,9 +1,10 @@
 package com.hwy.example;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.hwy.textview.XEditText;
 import com.hwy.textview.listener.OnDrawableClickListener;
@@ -12,16 +13,20 @@ public class EditActivity extends AppCompatActivity {
 
     private XEditText et;
 
+    private Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
         et = findViewById(R.id.et);
 
+        mContext = this;
+
         et.setOnDrawableClickListener(new OnDrawableClickListener() {
             @Override
             public void onDrawableLeftClickListener(TextView view) {
-                Log.e("TAG", "onDrawableLeftClick");
+                Toast.makeText(mContext, "DrawableLeftClick", Toast.LENGTH_SHORT).show();
             }
 
             @Override
